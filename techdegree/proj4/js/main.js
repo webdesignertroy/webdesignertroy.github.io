ddb78554eff71c763fbe4213a59a55e1793cd658
Troy Rowtham <sava.rowtham@yahoo.com>
2016-05-14 11:39:41 -0700

    Proj 4 - Modified Animation

--- techdegree/proj4/js/main.js
+++ techdegree/proj4/js/main.js
@@ -444,12 +444,15 @@ $('#searchbox').keyup(function(){
 	targetImg.each(function(){
 		searchThis = $(this).attr("alt").toLowerCase() + $(this).attr("title").toLowerCase();		
 		if (searchThis.indexOf(searchValue) === -1){
-			$(this).attr("class", "image_hide").parent().unwrap("<div class='col'></div>").wrap("<span class='col-hide'></span>"); 
-			$(this).parent().parent().hide(1000, function(){
-				$(this).attr("style", "display: none");
-			});
+			if( $(this).attr("class") !== "image_hide" ){
+				$(this).attr("class", "image_hide").parent().unwrap("<div class='col'></div>").wrap("<span class='col-hide'></span>"); 
+				$(this).parent().parent().hide(1000);
+			}
 		} else {
-			$(this).attr("class", "image").parent().unwrap("<span class='col-hide' style='display: none'></span>").wrap("<div class='col'></div>").show(1000);
+			if( $(this).attr("class") !== "image" ) {
+				$(this).attr("class", "image").parent().unwrap("<span class='col-hide'></span>").wrap("<div class='col'></div>");
+				$(this).parent().parent().show(1000);
+			}
 		}
 	});	
 	//No results
