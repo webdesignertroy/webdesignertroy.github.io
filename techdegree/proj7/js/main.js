e1975027abd12221edd6a70dc4ed439caac41dd0
webdesignertroy <sava.rowtham@yahoo.com>
2016-08-09 23:29:07 -0700

    Project 7 - Fixed HTML Error
    Fixed responsiveness of video

--- techdegree/proj7/js/main.js
+++ techdegree/proj7/js/main.js
@@ -24,6 +24,7 @@ MediaElementPlayer.prototype.buildloop = function(player, controls, layers, medi
 
 $(document).ready(function(){
 
+
 	/************************
 	Variables
 	*************************/
