78fa50045b81e4832900f31b42762c2a12aaa131
webdesignertroy <sava.rowtham@yahoo.com>
2016-09-13 07:36:57 -0700

    Project10 - Initial Upload

---
+++ techdegree/proj10/js/api.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*********************************
+   VARIABLES
+**********************************/
+	var $search = $("#search");
+	var $submit = $("#submit");
+
+
+
+});// end of ready
