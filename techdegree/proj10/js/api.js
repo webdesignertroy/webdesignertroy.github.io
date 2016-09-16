88694bf679e370220f4f25fe2623cb96eb3ae8d6
webdesignertroy <sava.rowtham@yahoo.com>
2016-09-15 20:48:52 -0700

    proj 10- overlay style part 3

--- techdegree/proj10/js/api.js
+++ techdegree/proj10/js/api.js
@@ -9,10 +9,24 @@
 	var $info = $('<div id="info"></div>');
 	var $cursorBorderLeft = $('<div id="cursor-border-left"></div>');
 	var $cursorBorderRight = $('<div id="cursor-border-right"></div>');
+	var $arrowLeft = $('<div id="arrow-left"></div>');
+	var $arrowRight = $('<div id="arrow-right"></div>');
 	var $resultLink = $("#container .result-link");
 	var $loader = $("#loader-background");
 
 	var details;
+
+	var createDetails = function(index) {
+		var imdbImage = 'http://img.omdbapi.com/?i=' + details[index].id + '&apikey=7fe29f8b'
+		var detailHTML = '<div data-id="' + details[index].id + '">';
+		detailHTML +='<p>' + details[index].title + '</p>\n';
+		if (details[index].image !== "N/A") {
+			detailHTML += '<div class="detail-img" style="background: url(' +imdbImage +')"></div>';
+			detailHTML += '</div>';
+		}
+		$info.html(detailHTML);
+		detailHTML = "";
+	}
 
 /*********************************
    API
@@ -45,7 +59,7 @@ $submit.on("click", function(e){
 				})
 			// 
 			if ( data.Poster !== "N/A" ) {
-				movieHTML += '<div class="result">\n';
+				movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
 				movieHTML += '<a href="#" class="result-link">\n';
 				var movieImage = '<img src="http://img.omdbapi.com/?i=' + data.imdbID + '&apikey=7fe29f8b" class="result-img"/>';
 				movieHTML += movieImage + '\n';
@@ -53,7 +67,7 @@ $submit.on("click", function(e){
 				movieHTML += '</a>\n';
 				movieHTML += '</div>\n';
 			} else {
-				movieHTML += '<div class="result">\n';
+				movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
 				movieHTML += '<a href="#" class="no-result-link">\n';
 				movieHTML += '<p class="link-header">Poster Not Available</p>\n';
 				movieHTML += '<p>' + data.Title + '</p>\n';
@@ -74,11 +88,26 @@ $submit.on("click", function(e){
 	$.getJSON(url, movieData, movieResults);
 });
 	// Add .show on result click
-	 $container.on("click", $(".result-link"), function(){
+	 $container.on("click", ".result", function(evt){
+	 	evt.preventDefault();
+	 	evt.stopPropagation();
+	 	// Append #overlay
 		$info.appendTo($overlay);
+		$arrowRight.appendTo("wrapper");
+		$arrowLeft.appendTo("wrapper");
 		$cursorBorderRight.appendTo($overlay);
 		$cursorBorderLeft.appendTo($overlay);
 		$overlay.addClass("show");
+
+		// Find correct data in details array
+		var currentId = $(this).attr("id");
+		$.each(details, function(i, data){
+			if ( currentId === data.id ) {
+				createDetails(i);
+			}
+		});
+
+		// Fade-in overlay
 		$overlay.animate({
 			opacity: 1
 		}, 500);
@@ -95,8 +124,43 @@ $submit.on("click", function(e){
 			$info.remove();
 			$cursorBorderRight.remove();
 			$cursorBorderLeft.remove();
+			$arrowRight.remove();
+			$arrowLeft.remove();
 		});
 		
 	});// end of on click
+
+	/*********************************
+	   NAVIGATION
+	**********************************/
+
+	// On right arrow click (bubbling event issues)
+	$("wrapper").on("click", "#arrow-right",function(){
+		var currentId = $info.find("div").attr("data-id");
+		$.each(details, function(i, data){
+			if( data.id === currentId ) {
+				if ( i < details.length - 1) {
+					createDetails(i + 1);
+				} else {
+					createDetails(0);
+				}
+			}
+		});
+	});
+
+
+	// On left arrow click (bubbling event issues)
+	$("wrapper").on("click", "#arrow-left",function(){
+		var currentId = $info.find("div").attr("data-id");
+		$.each(details, function(i, data){
+			if( data.id === currentId ) {
+				if ( i > 0) {
+					createDetails(i - 1);
+				} else {
+					createDetails(details.length - 1);
+				}
+			}
+		});
+	});
 
 });// end of ready
