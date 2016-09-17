79ba9c723430aee4f56389f5a58fc9c701b07df0
webdesignertroy <sava.rowtham@yahoo.com>
2016-09-16 21:23:38 -0700

    proj 10 - First Complete Draft

--- techdegree/proj10/js/api.js
+++ techdegree/proj10/js/api.js
@@ -1,3 +1,11 @@
+
+	// Function Declaration for GoogleAPI API (YouTube API)
+	function init() {
+		gapi.client.setApiKey("AIzaSyD1i2kQhJqH2NGeZqECn0KQwlibpE36NDc");
+		gapi.client.load("youtube", "v3", function(){
+			// yt api is ready
+		});
+	}
  $(document).ready(function(){
 /*********************************
    VARIABLES
@@ -16,22 +24,85 @@
 
 	var details;
 
+	/*********** Function Expressions ***********/
 	var createDetails = function(index) {
+
+	 	//API #2: YouTube Search:link
+
+	 	// prepare the request
+	 	var imdbHref = "http://www.imdb.com/title/" + details[index].id;
+	 	var searchYear = details[index].year.slice(0,4);
+	 	var searchPhrase = encodeURIComponent(details[index].title + " release date " + searchYear).replace(/%20/g, "+");
+	 	searchPhrase = searchPhrase.replace(/%3A/g, "");
+	 	searchPhrase = searchPhrase.replace("(", "");
+	 	searchPhrase = searchPhrase.replace(")", "");
+
+	 	var request = gapi.client.youtube.search.list({
+	 		part: "snippet",
+	 		chart: "mostPopular",
+	 		type: "video",
+	 		q: searchPhrase,
+	 		maxResults: 1,
+	 		order:"viewCount"
+	 	})
+
+	 	//execute the request
+	 	request.execute(function(response) {
+	 		if ( response.items.length > 0 ) {
+	 			$(".item").remove(); 
+		 		var results = response.result;
+		 		$.each(results.items, function(index,item) {
+		 			$.get("tpl/item.html", function(data){
+		 				$info.append(tplawesome(data, [{
+		 					"videoid": item.id.videoId,
+		 					"imdblink": imdbHref
+		 				}]));
+		 				resetVideoHeight();
+		 			});
+		 		});
+		 	} else {
+		 		$.get("tpl/no-item.html", function(data){
+		 			$info.append(tplawesome(data, [{
+		 				"message": "Sorry. No related video was found for this title."
+		 			}]));
+		 		});
+		 	}
+	 	});
+	 	$(window).on("resize", resetVideoHeight);
+
 		var imdbImage = 'http://img.omdbapi.com/?i=' + details[index].id + '&apikey=7fe29f8b'
 		var detailHTML = '<div data-id="' + details[index].id + '">';
-		detailHTML +='<p>' + details[index].title + '</p>\n';
-		if (details[index].image !== "N/A") {
-			detailHTML += '<div class="detail-img" style="background: url(' +imdbImage +')"></div>';
-			detailHTML += '</div>';
-		}
+		detailHTML +='<h2>' + details[index].title + ' [' + searchYear + ']</h2>\n';
 		$info.html(detailHTML);
 		detailHTML = "";
-	}
+	} // end createDetails
+
+	/*********** Function Declarations ***********/
+
+	// "Seperation of concerns" practice w/ tplawesome
+	function tplawesome(template, data) {
+		// initiate the result to the basic template
+		res = template;
+		// for each data key, replace the content of the brackets with the data
+		for(var i = 0; i < data.length; i++) {
+			res = res.replace(/\{\{(.*?)\}\}/g, function(match, j) { // some magic regex
+				return data[i][j];
+			})
+		}
+		return res;
+	} // end tplawesome
+
+	//  Responsive: Keep iframe ratio
+	function resetVideoHeight() {
+		$(".video").attr("height", $info.width() * 9/16);
+	}// end resetVideoHeight
 
 /*********************************
-   API
+   API OMDB
 **********************************/
 $submit.on("click", function(e){
+	searchValue = $.trim($search.val());
+
 	e.preventDefault();
 
 	// Disable search field and button
@@ -39,44 +110,54 @@ $submit.on("click", function(e){
 	$submit.attr("disabled", true);
 	$loader.addClass("show-loader");
 
-	// API #1
-	var url = "http://www.omdbapi.com/?apikey=7fe29f8b&s=" + $search.val();
+	// API #1: OMDB
+	var url = "http://www.omdbapi.com/?apikey=7fe29f8b&s=" + searchValue;
 
 	var movieData = {
-		title: $search.val()
+		title: searchValue
 	}
 	var movieResults = function(response) {
-		var movieHTML = '';
-		details = [];
-		$.each(response.Search, function(i, data){
-			//Create Details Object
-				details.push({
-					title: data.Title,
-					year: data.Year,
-					id: data.imdbID,
-					type: data.Type,
-					image: data.Poster
-				})
-			// 
-			if ( data.Poster !== "N/A" ) {
-				movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
-				movieHTML += '<a href="#" class="result-link">\n';
-				var movieImage = '<img src="http://img.omdbapi.com/?i=' + data.imdbID + '&apikey=7fe29f8b" class="result-img"/>';
-				movieHTML += movieImage + '\n';
-				movieHTML += '<div class="content">' + data.Title + ' (' + data.Year + ')</div>';
-				movieHTML += '</a>\n';
-				movieHTML += '</div>\n';
-			} else {
-				movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
-				movieHTML += '<a href="#" class="no-result-link">\n';
-				movieHTML += '<p class="link-header">Poster Not Available</p>\n';
-				movieHTML += '<p>' + data.Title + '</p>\n';
-				movieHTML += '<p>(' + data.Year + ')</p>\n';
-				movieHTML += '</a>\n';
-				movieHTML += '</div>\n';
-			}
-		});
-		$container.html(movieHTML);
+		if ( response.Response !== "False" ) {
+			var movieHTML = '';
+			details = [];
+			$.each(response.Search, function(i, data){
+				//Create Details Object
+					details.push({
+						title: data.Title,
+						year: data.Year,
+						id: data.imdbID,
+						type: data.Type,
+						image: data.Poster
+					})
+				// Build HTML in JavaScript (Dirty)
+				if ( data.Poster !== "N/A" ) {
+					movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
+					movieHTML += '<a href="#" class="result-link">\n';
+					var movieImage = '<img src="http://img.omdbapi.com/?i=' + data.imdbID + '&apikey=7fe29f8b" class="result-img"/>';
+					movieHTML += movieImage + '\n';
+					movieHTML += '<div class="content">' + data.Title + ' (' + data.Year + ')</div>';
+					movieHTML += '</a>\n';
+					movieHTML += '</div>\n';
+				} else {
+					movieHTML += '<div class="result" id="' + data.imdbID+ '">\n';
+					movieHTML += '<a href="#" class="no-result-link">\n';
+					movieHTML += '<p class="link-header">Poster Not Available</p>\n';
+					movieHTML += '<p>' + data.Title + '</p>\n';
+					movieHTML += '<p>(' + data.Year + ')</p>\n';
+					movieHTML += '</a>\n';
+					movieHTML += '</div>\n';
+				}
+			});
+			$container.html(movieHTML);
+		} else {
+			$container.html("");
+	 		$.get("tpl/no-movie.html", function(data){
+	 			$("#container").append(tplawesome(data, [{
+	 				"messageTitle": "This Title was Not Found",
+	 				"message": 'Please check your spelling (i.e. "Indiana Jone" instead of "Indiana Jones" ).'
+	 			}]));
+	 		});
+		}
 		$search.val("");
 	}
 	// Enable search field and button
@@ -86,11 +167,14 @@ $submit.on("click", function(e){
 
 	// call API #1
 	$.getJSON(url, movieData, movieResults);
+
 });
 	// Add .show on result click
 	 $container.on("click", ".result", function(evt){
 	 	evt.preventDefault();
 	 	evt.stopPropagation();
+
+
 	 	// Append #overlay
 		$info.appendTo($overlay);
 		$arrowRight.appendTo("wrapper");
@@ -161,6 +245,26 @@ $submit.on("click", function(e){
 				}
 			}
 		});
+	});
+	/*********************************
+	   ACESSIBILITY: KEYBOARD
+	**********************************/
+	$(this).keyup(function(e){
+	"use strict";
+		switch(e.keyCode) {
+			case 27:
+		//Fade out overlay when[ESC=27] is keyed.
+		$overlay.trigger("click");
+		break;
+		case 37:
+		//Advances slideshow left on left-arrow [37] key.
+			$("#arrow-left").trigger("click");			
+		break;
+		case 39:
+		//Advances slideshow right on right-arrow [39] key.
+			$("#arrow-left").trigger("click");
+		break;
+		}
 	});
 
 });// end of ready
