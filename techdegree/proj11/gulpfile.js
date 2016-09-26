0d15a78e8386832817f03b700fa4f8127d6d8586
webdesignertroy <sava.rowtham@yahoo.com>
2016-09-25 23:28:30 -0700

    proj-11 - First Draft Update

---
+++ techdegree/proj11/gulpfile.js
@@ -0,0 +1,78 @@
+"use strict";
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+}
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglifycss = require('gulp-uglifycss'),
+	uglify = require('gulp-uglify'),
+	image = require('gulp-image'),
+	rename = require('gulp-rename'),
+	watch = require('gulp-watch');
+
+gulp.task("concatCSS", function(){
+	return gulp.src([
+		'css/normalize.css',
+        'css/foundation.css',
+        'css/basics.css',
+        'css/menu.css',
+        'css/hero.css',
+        'css/photo-grid.css',
+        'css/modals.css',
+        'css/footer.css'
+		])
+	.pipe(concat('style.css'))
+	.pipe(gulp.dest('css'))
+});
+
+
+gulp.task("concatScripts", function(){
+	return gulp.src([
+		'js/fastclick.js',  
+		'js/foundation.js',
+		'js/foundation.equalizer.js',
+		'js/foundation.reveal.js',
+		'js/scripts.js'])
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest('js'))
+});
+
+gulp.task("minifyCSS", ['concatCSS'], function(){
+	return gulp.src("css/style.css")
+		.pipe(uglifycss())
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('css'))
+});
+
+gulp.task("minifyScripts",['concatScripts'], function(){
+	return gulp.src("js/app.js")
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('js'))
+});
+
+gulp.task("optimizeImg", function(){
+	gulp.src("img/*/*")
+		.pipe(image({
+	      pngquant: false,
+	      optipng: false,
+	      zopflipng: false,
+	      jpegRecompress: true,
+	      jpegoptim: false,
+	      mozjpeg: false,
+	      gifsicle: false,
+	      svgo: false,
+	      concurrent: 10
+	    }))
+		.pipe(gulp.dest('./dest/img'))
+});
+gulp.task('watchCSS', function(){
+	gulp.watch('css/*.css', ['minifyCSS']);
+	gulp.watch('js/*.js', ['minifyScripts']);
+});
+
+gulp.task('build', ['minifyCSS', 'minifyScripts'])	
+
+gulp.task('default', ['build']);
---
+++ techdegree/proj11/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	// Variables
+	var $imageThumb = $(".image a img");
+
+	// On image thumb click, replace thumb with larger image
+
+	//   Get large src string
+	$imageThumb.on("click", function(){
+		var $thumbSrc = $(this).attr("src");
+		var largerSrc = $thumbSrc.replace("dest/img/photos/thumbs/", "img/photos/");
+		var $dataInfo = $(this).parent().attr("data-reveal-id"); 
+		$("#" + $dataInfo).find("img").attr("src", largerSrc);
+	});
+});
