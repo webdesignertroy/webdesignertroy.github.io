fb8a85422a9be19f4e4c714d5db3ae23881ff48a
webdesignertroy <sava.rowtham@yahoo.com>
2016-09-26 08:58:53 -0700

    proj 11 - Optimize Large Images - max-width: 2500

--- techdegree/proj11/js/app.js
+++ techdegree/proj11/js/app.js
@@ -1404,7 +1404,7 @@ $(document).ready(function(){
 	//   Get large src string
 	$imageThumb.on("click", function(){
 		var $thumbSrc = $(this).attr("src");
-		var largerSrc = $thumbSrc.replace("dest/img/photos/thumbs/", "img/photos/");
+		var largerSrc = $thumbSrc.replace("dest/img/photos/thumbs/", "dest/img/photos/");
 		var $dataInfo = $(this).parent().attr("data-reveal-id"); 
 		$("#" + $dataInfo).find("img").attr("src", largerSrc);
 	});
