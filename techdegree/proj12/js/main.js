dc017b222ad60c5c4046cd017a601ff6febb2c55
webdesignertroy <sava.rowtham@yahoo.com>
2016-10-03 12:29:22 -0700

    proj 12 - Menu Second Draft

--- techdegree/proj12/js/main.js
+++ techdegree/proj12/js/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
+
+	/************************
+		VARIABLES
+	*************************/
+
+	var $wrapper = $("#wrapper");
+	var $navDiv = $("#nav");
+	var $navBar = $("#main");
 	var $menuLi = $("#main li a");
 	var $backTop = $("#back-top a");
+	var $menuReveal = $("#menu-reveal");
+	var mq = window.matchMedia('all and (max-width: 769px)');
+	var menuLength = 0;
 
 	/************************
 		FUNCTION EXPRESSION
 	*************************/
 
+	// Function: hide menu items
+	var hideMenu = function() {
+		$navBar.slideUp(700, "swing");
+	};
+
+	// Function: show menu items
+	var showMenu = function() {
+		$navBar.slideDown(700, "swing");
+	};
 	// Function: scrolls to 'targeted id' on page
 	var $scroll = function($hash, menuCount) {
 		$('html, body').animate({
@@ -17,7 +37,7 @@ $(document).ready(function(){
 	   NAVIGATION
 	*************************/	
 
- 	// On menu li click, scroll to targeted id
+ 	// On menu li click, scroll to 'targeted id'
  	$menuLi.on("click", function(e){
 
  		// prevent normal action
@@ -26,6 +46,24 @@ $(document).ready(function(){
  		// defines the href of the'targeted id' I'm looking for
  		var $link = $(this).attr("href");
  		$scroll($link, 0);
+
+ 		// counts menu items to determine menu height
+		mq.addListener(function(changed) {
+			if(changed.matches) {
+				menuLength = $menuLi.length;
+			} else {
+				$menuReveal.hide();
+				menuLength = 0;
+			}
+		});
+
+		// invokes hideMenu()
+		if (mq.matches) {
+			hideMenu();
+		}
+
+		// invokes $scroll()
+ 	 	$scroll($link, menuLength);
 
  	});
 
@@ -37,8 +75,97 @@ $(document).ready(function(){
 
  	 	// defines the href of the 'targeted id' I'm looking for
  	 	var $link = $(this).attr("href");
- 	 	$scroll($link, 0);
+
+		// counts menu items to determine menu height
+		mq.addListener(function(changed) {
+			if(changed.matches) {
+				menuLength = $menuLi.length;
+			} else {
+				$menuReveal.hide();
+				menuLength = 0;
+			}
+		});
+
+		// invokes hideMenu()
+		if (mq.matches) {
+			hideMenu();
+		}
+
+		// invokes $scroll()
+ 	 	$scroll($link, menuLength);
  	 });
+
+	// if javascript works, hide/show appropriate menus
+	if (mq.matches) {
+		$menuReveal.show();
+		$navBar.hide();
+		menuLength = $menuLi.length;
+	} else {
+		$menuReveal.hide();
+		menuLength = 0;
+	}
+
+ 	/************************
+	   EVENT LISTENERS
+	*************************/	
+
+	// On menu click, reveal or hide mobile navigation
+	$menuReveal.on("click", function(){
+		// toggles MenuReveal button
+		if( $navBar.css("display") === "none" ) {
+			showMenu();
+		} else {
+			hideMenu();
+		}
+	});
+
+	// If javascript is on and the media window changes, 
+	//    hide/show appropriate menus
+	mq.addListener(function(changed) {
+		if(changed.matches) {
+			$menuReveal.show();
+			$navBar.hide();
+			menuLength = $menuLi.length;
+		} else {
+			$menuReveal.hide();
+			$navBar.show();
+			menuLength = 0;
+		}
+	});
+
+	// Listen for scroll pass 440px then fix main navigation
+	if( $(window).height() !== $(document).height() ) {
+		$(window).on("scroll", function(){
+		 	var scrollWin = $(this).scrollTop();
+		 	if ( scrollWin > 440 ) {
+		 		$navDiv.addClass("fix-menu");
+		 		$wrapper.addClass("add-lost-height");
+		 		$("#back-top").removeClass("hide-div");
+		 	} else {
+		 		$navDiv.removeClass("fix-menu");
+		 		$wrapper.removeClass("add-lost-height");
+		 		$("#back-top").addClass("hide-div");
+
+		 	}
+		});
+	}
+	// Listen for scroll pass 100px then fix #back-top
+	if( $(window).height() !== $(document).height() ) {
+		$(window).on("scroll", function(){
+		 	var scrollWin = $(this).scrollTop();
+		 	if ( scrollWin > 200 ) {
+		 		$("#back-top").removeClass("hide-div");
+		 	} else {
+		 		$("#back-top").addClass("hide-div");
+
+		 	}
+		});
+	}
+
+
+ 	/************************
+	   HEADER INTERACTIVE 
+	*************************/	
 
 	// On left-hemisphere hover in header
 	$("#left-trigger").mouseenter(function(){
